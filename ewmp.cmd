0e73567bec911a1b541ef7ccac0774915d8fb4d4
Lee Atkinson <lee@rocketindigo.com>
2014-12-17 13:34:26 +0000

    Added comment to the command file

--- ewmp.cmd
+++ ewmp.cmd
@@ -1,3 +1,5 @@
+REM Sets the session's PATH variable to that of the machine, and executes a command
+
 @ECHO OFF
 FOR /F "tokens=3,*" %%a IN ('REG QUERY "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" /v PATH') DO PATH %%a%%b
 %*
